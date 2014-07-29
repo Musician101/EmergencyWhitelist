82774169279563fe4c9c0ffb5702bfa71a8ac700
Musician101 <yellowfireproductions@yahoo.com>
2014-07-28 23:45:08 -0700

    More comment removal.

--- src/musician101/emergencywhitelist/util/RunKickMethod.java
+++ src/musician101/emergencywhitelist/util/RunKickMethod.java
@@ -6,17 +6,8 @@ import musician101.emergencywhitelist.runnables.KickPlayers;
 
 import org.bukkit.Bukkit;
 
-/**
- * Runs when '/ewl toggle' is executed.
- * 
- * @author Musician101
- */
 public class RunKickMethod 
 {
-	/**
-	 * @param plugin Reference to the plugin's main class.
-	 * @param enabled References the 'enabled' option in the config.
-	 */
 	public RunKickMethod(EmergencyWhitelist plugin, boolean enabled)
 	{
 		plugin.getLogger().info(Messages.getWhitelistEnabled(enabled));
